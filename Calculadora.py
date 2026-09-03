3543719465fe551a2158b4e3ce51c0053400b0d2
Ezequiel <eb66963@gmail.com>
2026-09-03 14:47:13 -0300

    Calculadora finalizada

--- Calculadora.py
+++ Calculadora.py
@@ -17,26 +17,29 @@ while opcao != 0:
                 num1 = float(input("Digite o primeiro número: "))
                 num2 = float(input("Digite o segundo número: "))
 
-        if opcao == 1:
-                resultado = num1 + num2
-                print(f"O resultado da soma é: {resultado}")
+                if opcao == 1:
+                        resultado = num1 + num2
+                        print(f"O resultado da soma é: {resultado}")
 
 
-        elif opcao == 2:
-                resultado = num1 - num2
-                print(f"O resultado da subtração é: {resultado}")
+                elif opcao == 2:
+                        resultado = num1 - num2
+                        print(f"O resultado da subtração é: {resultado}")
 
 
-        elif opcao == 3:
-                resultado = num1 * num2
-                print(f"O resultado da multiplicação é: {resultado}")
+                elif opcao == 3:
+                        resultado = num1 * num2
+                        print(f"O resultado da multiplicação é: {resultado}")
 
 
-        elif opcao == 4:
-                if num2 != 0:
-                    resultado = num1 / num2
-                    print(f"O resultado da divisão é: {resultado}")
+                elif opcao == 4:
+                        if num2 != 0:
+                                resultado = num1 / num2
+                                print(f"O resultado da divisão é: {resultado}")
 
 
                 else:
                     print("Erro: Divisão por zero não é permitida.")
+
+        else:
+                print("Opção inválida. Por favor, escolha uma opção válida.")
