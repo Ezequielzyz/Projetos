42df089f0b14c64502b69bdd23f6c7dd7b0f38e3
Ezequiel <eb66963@gmail.com>
2026-09-03 16:07:05 -0300

    Melhorias

--- Jogo de números.py
+++ Jogo de números.py
@@ -7,23 +7,27 @@ numero_sorteado = random.randint(1, 100)
 
 diferenca = abs(numero_escolhido - numero_sorteado)
 
+tentativas = 0
+
 
 while numero_escolhido != numero_sorteado:
+        
+    tentativas += 1
+    diferenca = abs(numero_escolhido - numero_sorteado)
 
-        if numero_escolhido > 100 or numero_escolhido < 1:
-            print("Escolha um número válido entre 1 e 100")
-            numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
-            diferenca = abs(numero_escolhido - numero_sorteado)
+    if numero_escolhido > 100 or numero_escolhido < 1:
+        print("Escolha um número válido entre 1 e 100")
+        numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
 
-        elif diferenca >= 1 and diferenca <=10:
-                 print("Você está perto!")
-                 numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
-                 diferenca = abs(numero_escolhido - numero_sorteado)
+    elif diferenca >= 1 and diferenca <=10:
+        print("Você está perto!")
+        numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
 
-        else:
-               print("Você está longe!")
-               numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
-               diferenca = abs(numero_escolhido - numero_sorteado)
-               
+    elif diferenca > 10:
+        print("Você está longe!")
+        numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
+
 if numero_escolhido == numero_sorteado:
-     print("Parabéns! Você acertou o número sorteado:", numero_sorteado)
+    tentativas += 1
+    print("Parabéns! Você acertou o número sorteado:", numero_sorteado)
+    print("Número de tentativas:", tentativas)
