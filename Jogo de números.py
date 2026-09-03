9c655ab005822c86c9d16f03b39d88b80c8f5d8f
Ezequiel <eb66963@gmail.com>
2026-09-03 16:29:03 -0300

    Jogo finalizado

--- Jogo de números.py
+++ Jogo de números.py
@@ -13,18 +13,26 @@ tentativas = 0
 while numero_escolhido != numero_sorteado:
         
     tentativas += 1
-    diferenca = abs(numero_escolhido - numero_sorteado)
+    diferenca = (numero_escolhido - numero_sorteado)
 
     if numero_escolhido > 100 or numero_escolhido < 1:
         print("Escolha um número válido entre 1 e 100")
         numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
 
-    elif diferenca >= 1 and diferenca <=10:
-        print("Você está perto!")
+    elif diferenca >= -10 and diferenca <= -1:
+        print("Você está perto, chute mais alto!")
         numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
 
-    elif diferenca > 10:
-        print("Você está longe!")
+    elif diferenca >= 1 and diferenca <= 10:
+        print("Você está perto, chute mais baixo!")
+        numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
+
+    elif diferenca >= -99 and diferenca <= -11:
+        print("Você está longe, chute mais alto!")
+        numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
+    
+    elif diferenca >= 11 and diferenca <= 99:
+        print("Você está longe, chute mais baixo!")
         numero_escolhido = int(input("Escolha um número entre 1 e 100: "))
 
 if numero_escolhido == numero_sorteado:
