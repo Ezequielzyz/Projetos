e0bfdc1c6e591ae9b65ec23ffeb3d2d392697bfd
Ezequiel <eb66963@gmail.com>
2026-09-11 15:57:08 -0300

    Criando um save em arquivo

--- estudo.py
+++ estudo.py
@@ -1,19 +1,31 @@
-print("Gerenciador de Finanças Pessoais")
+import csv
+import os
+
+print("\nGerenciador de Finanças Pessoais\n")
+
+arquivos_dados = "historico_financas"
 
 saldo = 0
 historico = []
 
+if os.path.exists(arquivos_dados)
+
 while True:
-    print("1. Cadastar Ganhos")
-    print("2. Cadastrar Gastos")
-    print("3. Ver histórico")
-    print("4. Fechar programa")
-    escolha = int(input("Escolha uma opção: "))
+    try:
+        print("1. Cadastar Ganhos")
+        print("2. Cadastrar Gastos")
+        print("3. Ver histórico")
+        print("4. Fechar programa")
+        escolha = int(input("\nEscolha uma opção: "))
+
+    except ValueError:
+        print("\nInsira um valor válido!\n")
+        continue
     
 
     if escolha == 1:
         
-        tipo_ganho = input("Qual a origem do ganho? :")
+        tipo_ganho = input("\nQual a origem do ganho? :")
 
         while True:
 
@@ -21,16 +33,16 @@ while True:
                 valor = float(input("Valor do ganho? :"))
                 transacao = {"Tipo": "Ganho", "Origem": tipo_ganho, "Valor": valor}
                 historico.append(transacao)
-                print("Ganho armazenado com sucesso!")
+                print("\nGanho armazenado com sucesso!")
                 saldo += transacao["Valor"]
-                print(f"Seu saldo é: {saldo}!")
+                print(f"\nSeu saldo é: R${saldo:.2f}!\n")
                 break
             
             except ValueError:
                 print("Insira um valor válido!")
 
     elif escolha == 2:
-        tipo_despesa = input("Qual origem da despesa? :")
+        tipo_despesa = input("\nQual origem da despesa? :")
 
         while True:
             
@@ -38,9 +50,9 @@ while True:
                 valor = float(input("Valor da despesa:"))
                 transacao = {"Tipo": "Despesa", "Origem": tipo_despesa, "Valor": valor}
                 historico.append(transacao)
-                print("Despesa armazenada com sucesso!")
+                print("\nDespesa armazenada com sucesso!")
                 saldo -= transacao["Valor"]
-                print(f"Seu saldo é: {saldo}!")
+                print(f"\nSeu saldo é: R${saldo:.2f}!\n")
                 break
             
             except ValueError:
@@ -54,8 +66,21 @@ while True:
             valor = transacao["Valor"]
 
             if tipo == "Ganho":
-                print(f" [+] R${valor} {origem}")
+                print(f"\n [+] R${valor:.2f} - {origem}\n")
+
+            else:
+                print(f"\n [-] R${abs(valor):.2f} - {origem}\n")
+
+        if saldo > 0:
+            print(f"\nSALDO: [+] R${saldo:.2f}\n")
+        
+        else:
+            print(f"\nSALDO: [-] R${abs(saldo):.2f}\n")
+        
 
     elif escolha == 4:
         print("Fechando...")
         break
+
+    else:
+        print("\nInsira um valor válido\n")
